51f1773aa60b8a286376d1ba5fda50b5a17dae04
mereaux <dominique.mereaux@qualifiez.fr>
2025-11-14 13:57:45 +0100

    ajout test variabilise

--- src/test/java/testFrais/TestCalculFrais.java
+++ src/test/java/testFrais/TestCalculFrais.java
@@ -9,11 +9,6 @@ class TestCalculFrais {
 		CalculFrais f = new CalculFrais();
 		assertEquals(f.montant(600.0),0.0,0.0001);
 	}
-	@Test
-	void test3() throws MonException {
-		CalculFrais f = new CalculFrais();
-		assertEquals(f.montant(100.0),20.0,0.0001);
-	}
 	@Test()
 	void test2() throws MonException {
 			assertThrows(MonException.class, () -> {
---
+++ src/test/java/testFrais/TestVariabilise.java
@@ -0,0 +1,18 @@
+package testFrais;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+class TestVariabilise {
+
+	@ParameterizedTest
+	@CsvFileSource(resources="donnees.csv",numLinesToSkip=1)
+	void test(double total, double frais) throws MonException {
+		CalculFrais f = new CalculFrais();
+		assertEquals(f.montant(total),frais,0.0001);
+	}
+
+}
